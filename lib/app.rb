1b1a179f27422439e987b40fb0bfb2b30bb8847d
Steven <Steven@Stevens-iMac.local>
2020-03-30 16:38:03 +0100

    Boarder added, with catto

--- lib/app.rb
+++ lib/app.rb
@@ -9,6 +9,12 @@ get '/secret' do
     'This is a secret page, now with shotgun??' 
 end
 
+#get '/catto' do
+#  '<h1> <div style= 'border: 3px dashed red'> <img src='http://bit.ly/1eze8aE'> </div>" </h1>' 
+#end
+
 get '/catto' do
-  'This is where the cattos live' 
+  "<div>
+    <img style='border: 3px dashed red' src='http://bit.ly/1eze8aE'>
+    </div>"
 end
