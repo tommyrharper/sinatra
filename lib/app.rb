ca5c731a71b14fe18ef7c6afc732c371b0984c6b
Steven <Steven@Stevens-iMac.local>
2020-03-30 16:03:12 +0100

    added the cattos

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
-
+set :session_secret, 'super secret'
 
 get '/' do
   "Steven and Tom are amazing!"
+end
+
+get '/secret' do
+    'This is a secret page, now with shotgun??' 
+end
+
+get '/catto' do
+  'This is where the cattos live' 
 end
